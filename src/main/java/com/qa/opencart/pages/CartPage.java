2666ad58bde136d0cfd7e4da50ff6741e337f119
Abhishek <abhishekreddy0907@outlook.com>
2022-03-15 21:36:32 +0530

    Cart Page Updated as per review

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,9 +1,14 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
+	
+	By cartbtn = By.id("test");
+	
 	public void cart() {
-		System.out.println("Cart Page");
+		System.out.println("Cart Page with 2 items");
 
 	}
 }
